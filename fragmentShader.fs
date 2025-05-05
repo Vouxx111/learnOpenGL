8089d7c70b58ad662c5cf8263f95ac4f4341be50
Vouxx <ejs28605@proton.me>
2025-05-05 18:40:44 -0400

    Cube Render (Fully Functional)

--- fragmentShader.fs
+++ fragmentShader.fs
@@ -1,6 +1,5 @@
 #version 330 core
-   
-in vec3 vertexColor;
+
 in vec2 TexCoord;
 
 out vec4 FragColor;
@@ -10,5 +9,5 @@ uniform sampler2D texture2;
 uniform float mixValue;
 
 void main() {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue) * vec4(vertexColor, 1.0); // Add full opacity manually
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue); // Add full opacity manually
 }
--- main.cpp
+++ main.cpp
@@ -84,8 +84,6 @@ int main() {
 	}
 
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	Shader ourShader("C:/Users/iflyf/OneDrive/Documents/GitHub/learnOpenGL/vertexShader.vs", "C:/Users/iflyf/OneDrive/Documents/GitHub/learnOpenGL/fragmentShader.fs"); //Declare New External Shader	
 
@@ -103,42 +101,67 @@ int main() {
 
 	// Vertex array (4 unique vertices for the square)
 	float vertices[] = {
-		// positions           // colors       // tex coords
-		-0.5f, -0.5f, -0.5f,    0.0f, 0.0f, 0.0f,   0.0f, 0.0f, // 0
-		 0.5f, -0.5f, -0.5f,    0.0f, 0.0f, 0.0f,   1.0f, 0.0f, // 1
-		 0.5f,  0.5f, -0.5f,    0.0f, 0.0f, 0.0f,   1.0f, 1.0f, // 2
-		-0.5f,  0.5f, -0.5f,    0.0f, 0.0f, 0.0f,   0.0f, 1.0f, // 3
-		-0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 0.0f,   0.0f, 0.0f, // 4
-		 0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 0.0f,   1.0f, 0.0f, // 5
-		 0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 0.0f,   1.0f, 1.0f, // 6
-		-0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 0.0f,   0.0f, 1.0f  // 7
-	};
-
-	// Indices to form two triangles from the 4 vertices
-	unsigned int indices[] = {
 		// back face
-		0, 1, 2,
-		2, 3, 0,
+		-1.0f, -1.0f, -1.0f,  0.0f, 0.0f,
+		 1.0f, -1.0f, -1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f, -1.0f,  1.0f, 1.0f,
+		 1.0f,  1.0f, -1.0f,  1.0f, 1.0f,
+		-1.0f,  1.0f, -1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f,  0.0f, 0.0f,
 
 		// front face
-		4, 5, 6,
-		6, 7, 4,
+		-1.0f, -1.0f,  1.0f,  0.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f,  1.0f, 1.0f,
+		 1.0f,  1.0f,  1.0f,  1.0f, 1.0f,
+		-1.0f,  1.0f,  1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f,  1.0f,  0.0f, 0.0f,
 
 		// left face
-		0, 3, 7,
-		7, 4, 0,
+		-1.0f,  1.0f,  1.0f,  1.0f, 0.0f,
+		-1.0f,  1.0f, -1.0f,  1.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f,  1.0f,  0.0f, 0.0f,
+		-1.0f,  1.0f,  1.0f,  1.0f, 0.0f,
 
 		// right face
-		1, 5, 6,
-		6, 2, 1,
+		 1.0f,  1.0f,  1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f, -1.0f,  1.0f, 1.0f,
+		 1.0f, -1.0f, -1.0f,  0.0f, 1.0f,
+		 1.0f, -1.0f, -1.0f,  0.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f,  0.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f,  1.0f, 0.0f,
 
-		// bottom face
-		0, 1, 5,
-		5, 4, 0,
+		 // bottom face
+		 -1.0f, -1.0f, -1.0f,  0.0f, 1.0f,
+		  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,
+		  1.0f, -1.0f,  1.0f,  1.0f, 0.0f,
+		  1.0f, -1.0f,  1.0f,  1.0f, 0.0f,
+		 -1.0f, -1.0f,  1.0f,  0.0f, 0.0f,
+		 -1.0f, -1.0f, -1.0f,  0.0f, 1.0f,
 
-		// top face
-		3, 2, 6,
-		6, 7, 3
+		 // top face
+		 -1.0f,  1.0f, -1.0f,  0.0f, 1.0f,
+		  1.0f,  1.0f, -1.0f,  1.0f, 1.0f,
+		  1.0f,  1.0f,  1.0f,  1.0f, 0.0f,
+		  1.0f,  1.0f,  1.0f,  1.0f, 0.0f,
+		 -1.0f,  1.0f,  1.0f,  0.0f, 0.0f,
+		 -1.0f,  1.0f, -1.0f,  0.0f, 1.0f
+	};
+
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,   0.0f,   0.0f),
+		glm::vec3(4.0f,  10.0f, -30.0f),
+		glm::vec3(-3.0f,  -4.4f,  -5.0f),
+		glm::vec3(-7.6f,  -4.0f, -24.6f),
+		glm::vec3(4.8f,  -0.8f,  -7.0f),
+		glm::vec3(-3.4f,   6.0f, -15.0f),
+		glm::vec3(2.6f,  -4.0f,  -5.0f),
+		glm::vec3(3.0f,   4.0f,  -5.0f),
+		glm::vec3(3.0f,   0.4f,  -3.0f),
+		glm::vec3(-2.6f,   2.0f,  -3.0f)
 	};
 
 
@@ -149,30 +172,21 @@ int main() {
 
 
 	// Generate buffers for vertices and indices
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);  // bind VAO first
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);  // bind and fill VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Bind and fill EBO (element buffer object)
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 	// Position attribute (location = 0)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// Color attribute (location = 1)
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)( 3* sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
 
 	// Good practice: unbind everything
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -240,19 +254,6 @@ int main() {
 
 
 	// ------------------------------------------------------------- //
-	//                       MATRIX SETUP                            //
-	// ------------------------------------------------------------- //
-
-
-	
-
-	
-
-	// Call resize callback once manually to initialize projection matrix
-	
-
-
-	// ------------------------------------------------------------- //
 	//                          RENDER LOOP                          //
 	// ------------------------------------------------------------- //
 
@@ -285,9 +286,8 @@ int main() {
 		glm::mat4 view = glm::mat4(1.0f); // Up direction
 		glm::mat4 projection = glm::mat4(1.0f);
 
-		model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
-		projection = glm::perspective(glm::radians(45.0f), static_cast<float>(screenWidth) / screenHeight, 0.1f, 100.0f);
+		projection = glm::perspective(glm::radians(90.0f), static_cast<float>(screenWidth) / screenHeight, 0.1f, 100.0f);
 
 		// Get uniform locations
 		unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
@@ -295,14 +295,31 @@ int main() {
 		unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
 
 		//Set Matrixes
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 
 
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);	// Draw 2 triangles from the index buffer
+		for (unsigned int x = 0; x < 10; x++)	{
+
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[x]);
+
+			if (x % 3 == 0)	{
+				model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+			}
+			else	{
+				float angle = 20.0f * x;
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			}
+			
+
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 
 		glfwSwapBuffers(window);
